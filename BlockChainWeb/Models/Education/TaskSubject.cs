f035134c9ad352954ad3b5c99c6156fb4d1e1a70
YuraPunchyk <36701820+YuraPunchyk@users.noreply.github.com>
2020-03-03 13:53:41 +0200

    upgrate 9

--- BlockChainWeb/Models/Education/TaskSubject.cs
+++ BlockChainWeb/Models/Education/TaskSubject.cs
@@ -1,11 +1,12 @@
-﻿
-
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BlockChainWeb.Models.Education {
-	public class Subject {
+	public class TaskSubject {
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public List<TaskSubject> TaskSubjects { get; set; }
+		public float MaxValuation { get; set; }
 	}
 }
